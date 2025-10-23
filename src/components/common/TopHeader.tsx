0e0bcc242c4ea7d91d205ee03ea39b6ce2793489
iftykhar <s.m.ifty49@gmail.com>
2025-10-24 05:37:09 +0600

    feat: topheader working

--- src/components/common/TopHeader.tsx
+++ src/components/common/TopHeader.tsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react";
+import {
+  Facebook,
+  Instagram,
+  Mail,
+  MapPin,
+  PhoneForwarded,
+  X,
+} from "lucide-react";
+import Link from "next/link";
 
 export default function TopHeader() {
   return (
     <div>
-        This is TopHeader Page
+      <section className="bg-primary relative w-full font-poppins">
+        <div className="container mx-auto">
+          <button className="absolute md:right-2 md:top-4 right-4 top-5 sm:right-2 sm:top-5 text-white flex items-center hover:text-red-500 transition">
+            <X className="cursor-pointer hover:text-white" size={20} />
+          </button>
+
+          <div className="flex flex-col md:flex-row justify-between items-center py-2 md:py-4 gap-2 md:gap-0 text-sm md:text-base">
+            {/* Left Section */}
+            <div className="message flex flex-col sm:flex-row justify-between items-center sm:items-start text-center sm:text-left gap-2 sm:gap-6 px-2">
+              <div className="flex items-center text-white gap-2 sm:gap-3">
+                <PhoneForwarded className="text-white w-4 h-4 sm:w-4 sm:h-4" />
+                <p className="text-[10px] sm:text-xs md:text-sm">654564</p>
+              </div>
+              <div className="flex items-center text-white gap-2 sm:gap-3 sm:text-xs md:text-sm">
+                <Mail className="text-white w-4 h-4 sm:w-4 sm:h-4" />
+                <a
+                  href="mailto:scubastevenar@gmail.com"
+                  className="hover:underline sm:text-xs md:text-sm"
+                >
+                  example@mail.com
+                </a>
+              </div>
+              <div className="flex items-center text-white gap-2 sm:gap-3 sm:text-xs md:text-sm">
+                <MapPin className="text-white w-4 h-4 sm:w-5 sm:h-5" />
+                <p className="text-[10px] sm:text-xs md:text-sm">
+                  vgbhnjmklkjhbnjm
+                </p>
+              </div>
+            </div>
+
+            {/* Right Section */}
+            <div className="phone-number flex flex-row items-center gap-2 sm:gap-3 mt-2 md:mt-0 text-[10px] sm:text-xs md:text-sm">
+              {/* <p className="text-white">Follow Us:</p> */}
+              <div className="flex gap-2 text-white sm:gap-3 sm:text-xs md:text-sm">
+                <Link href="https://www.google.com" target="_blank">
+                  <Facebook className="w-4 h-4 sm:w-4 sm:h-4 cursor-pointer" />
+                </Link>
+                <Link href="https://www.google.com" target="_blank">
+                  <Instagram className="w-4 h-4 sm:w-4 sm:h-4" />
+                </Link>
+                {/* <Link href="https://x.com/" target="_blank">
+                <Twitter className="w-3 h-3 sm:w-4 sm:h-4" />
+              </Link> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
