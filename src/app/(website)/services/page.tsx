035c50c23533afd582dbc2d235d8a3addd4fbe4c
RashedulHaqueRasel1 <rashedulhaquerasel1@gmail.com>
2025-10-24 05:34:57 +0600

    feat: add Hero component to About, Contact, Services pages
    Also configure Tailwind CSS for custom fonts

--- src/app/(website)/services/page.tsx
+++ src/app/(website)/services/page.tsx
@@ -1,12 +1,17 @@
-import FAQ from '@/components/sections/ServicesPage/FAQ'
-import Treatments from '@/components/sections/ServicesPage/Treatments'
-import React from 'react'
+import Hero from "@/components/Reusable/Hero";
+import FAQ from "@/components/sections/ServicesPage/FAQ";
+import Treatments from "@/components/sections/ServicesPage/Treatments";
+import React from "react";
 
 export default function page() {
   return (
     <div>
-        <Treatments />
-         <FAQ />
+      <Hero
+        heading="Our Signature Services"
+        subHeading="Discover expertly crafted experiences designed to delight your senses from artisanal flavors to unforgettable ambiance, we bring excellence to every moment."
+      />
+      <Treatments />
+      <FAQ />
     </div>
-  )
+  );
 }
